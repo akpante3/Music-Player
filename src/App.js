884973a27fd5eb012a7d598a712f6d8a26ad6004
akpante3 <obije.victor5@gmail.com>
2020-05-30 02:02:34 +0100

    Nav bar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from 'react';
 import styled from 'styled-components'
 import './App.css';
 import Sidebar from './components/sidebar'
+import Nav from './components/nav'
 
 function App() {
   const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
   font-family: 'Nunito', sans-serif;`;
+
+  const MainContainer = styled.div`
+    width: 100%
+  `
+
 
   return (
     <Wrapper>
       <Sidebar />
+      <MainContainer>
+        <Nav />
+      </MainContainer>
     </Wrapper>
   );
 }
--- src/style/color.js
+++ src/style/color.js
@@ -1,1 +1,3 @@
 export const sidebarBg = '#3e3e59'
+export const textcolor = '#d9d9da'
+export const navBg = '#36374d'
